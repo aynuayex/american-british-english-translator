633afebf371b49b21aaada8567def67d76e43cfb
Ayne Abreham Alemayehu <aynuman19@gmail.com>
2025-03-26 10:53:22 +0000

    feat: done with api and controller

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,53 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    // using static method because we can't use `this` inside forEach 
+    // since it does not refer to the class instance
+    static toTitleCase(str) {
+        return str.replace(/\b\w/, char => char.toUpperCase());
+    }
+    
+    americanToBritish (text) {
+        const textArray = text.split(" ");
 
+        textArray.forEach((word, index) => {
+            if(word.includes(":")) {
+                textArray[index] = `<span class="highlight">${word.replace(/:/, ".")}</span>`
+            }
+            if(Object.keys(americanOnly).includes(word)) {
+                textArray[index] = `<span class="highlight">${americanOnly[word]}</span>`
+            }
+            if(Object.keys(americanToBritishSpelling).includes(word)) {
+                textArray[index] = `<span class="highlight">${americanToBritishSpelling[word]}</span>`
+            }
+            if(Object.keys(americanToBritishTitles).includes(word.toLowerCase())) {
+                textArray[index] = `<span class="highlight">${Translator.toTitleCase(americanToBritishTitles[word.toLowerCase()])}</span>`
+            }
+        })
+        if(textArray.join(" ") === text) return "Everything looks good to me!"
+        return textArray.join(" ")
+    }
+
+    britishToAmerican (text) {
+        const textArray = text.split(" ");
+
+        textArray.forEach((word, index) => {
+            if(word.includes(".")) {
+                textArray[index] = `<span class="highlight">${word.replace(/\./, ":")}</span>`
+            }
+            if(Object.keys(britishOnly).includes(word)) {
+                textArray[index] = `<span class="highlight">${britishOnly[word]}</span>`
+            }
+            if(Object.values(americanToBritishSpelling).includes(word)) {
+                textArray[index] = `<span class="highlight">${Object.keys(americanToBritishSpelling).find(key => americanToBritishSpelling[key] == word)}</span>`
+            }
+            if(Object.values(americanToBritishTitles).includes(word.toLowerCase())) {
+                textArray[index] = `<span class="highlight">${Translator.toTitleCase(Object.keys(americanToBritishTitles).find(key => americanToBritishTitles[key] == word.toLowerCase()))}</span>`
+            }
+        })
+        if(textArray.join(" ") === text) return "Everything looks good to me!"
+        return textArray.join(" ")
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,26 @@ const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
   
-  const translator = new Translator();
+  const {americanToBritish, britishToAmerican } = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+
+      if(text === undefined || !locale) {
+        return res.json({ error: 'Required field(s) missing' })
+      }
+
+      if(text === "") {
+        return res.json({ error: 'No text to translate' })
+      }
+
+      if(locale !== "american-to-british" && locale !== "british-to-american" ) {
+        return res.json({ error: 'Invalid value for locale field' })
+      }
+
+      const translation = (locale === "american-to-british") ? americanToBritish(text) : britishToAmerican(text)
+      console.log({ text, translation})
+      return res.json({ text, translation})
     });
 };
